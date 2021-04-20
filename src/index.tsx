8c012ce6dd5302901ccde422a02c8374f3aee870
Lam Nguyen <lnguyendev@gmail.com>
2021-04-19 21:51:49 -0500

    Set up Esbuild along with custom plugins to handle onResolve and onLoad

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,59 @@
-import React from 'react';
+import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useEffect, useState, useRef } from 'react';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
+
+export const App = () => {
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
+    });
+  }
+
+  const onClick = async () => {
+    if (!ref.current) return;
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [
+        unpkgPathPlugin(),
+        fetchPlugin(input)
+      ],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window'
+      }
+    });
+
+    console.log('result: ', result);
+
+    setCode(result.outputFiles[0].text);
+  }
+
+  return (
+    <div>
+      <textarea onChange={e => setInput(e.target.value)} value={input}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  )
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
