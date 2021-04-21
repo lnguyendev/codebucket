09a56c5ae38f3506c7b0e31a69539c1628b679b3
Lam Nguyen <lnguyendev@gmail.com>
2021-04-20 22:19:13 -0500

    Run compiled code in an iframe

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 export const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   useEffect(() => {
     startService();
@@ -23,6 +23,8 @@ export const App = () => {
   const onClick = async () => {
     if (!ref.current) return;
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -37,10 +39,28 @@ export const App = () => {
       }
     });
 
-    console.log('result: ', result);
-
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   }
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (e) {
+              document.getElementById('root').innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + e + '</div>';
+              console.error(e);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -48,7 +68,7 @@ export const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html}></iframe>
     </div>
   )
 };
