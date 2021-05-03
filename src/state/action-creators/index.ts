711109cfe624fb5ec94afd1cc8065b61459b95ef
Lam Nguyen <lnguyendev@gmail.com>
2021-05-02 21:23:59 -0500

    Wired up bundling in redux

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,5 +1,7 @@
+import { Dispatch } from 'redux';
 import { ActionType } from '../action-types';
 import {
+  Action,
   UpdateCellAction,
   DeleteCellAction,
   MoveCellAction,
@@ -7,6 +9,7 @@ import {
   Direction,
 } from '../actions';
 import { CellTypes } from '../cell';
+import bundle from '../../bundler';
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -37,4 +40,25 @@ export const insertCellAfter = (
     type: ActionType.INSERT_CELL_AFTER,
     payload: { id, type: cellType },
   };
+};
+
+export const createBundle = (cellId: string, input: string) => async (
+  dispatch: Dispatch<Action>
+) => {
+  dispatch({
+    type: ActionType.BUNDLE_START,
+    payload: {
+      cellId,
+    },
+  });
+
+  const result = await bundle(input);
+
+  dispatch({
+    type: ActionType.BUNDLE_COMPLETE,
+    payload: {
+      cellId,
+      bundle: result,
+    },
+  });
 };
